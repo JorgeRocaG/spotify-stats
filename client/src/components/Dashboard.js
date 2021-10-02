f82b558c5c8b8dd9770ab8087e225605c0eccf09
Jorge <jorgeroca1996@gmail.com>
2021-10-03 01:01:22 +0200

    Remove User component (might re-use it later)

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,11 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
-import User from "./User";
 import TopArtists from "./TopArtists";
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   return (
     <div className="container">
-      <User accessToken={accessToken} />
       <TopArtists accessToken={accessToken} />
     </div>
   );
