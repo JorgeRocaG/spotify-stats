02b3871d1935398787044b5576339b8cbc00b97a
Jorge <jorgeroca1996@gmail.com>
2021-10-03 01:02:25 +0200

    Add a container to the Dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,16 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
+
 import TopArtists from "./TopArtists";
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
+
   return (
-    <div className="container">
-      <TopArtists accessToken={accessToken} />
+    <div className="my-4">
+      <div className="container mx-auto px-2 md:px-0">
+        <TopArtists accessToken={accessToken} />
+      </div>
     </div>
   );
 }
